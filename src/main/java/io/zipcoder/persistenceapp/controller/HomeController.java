9fa056f0a7a54c007fe5b297fc8e2303fb2dccf9
Carolynn Vansant <cvansant@MacBook-Pro-7.local>
2018-04-11 19:08:47 -0400

    small changes

--- src/main/java/io/zipcoder/persistenceapp/controller/HomeController.java
+++ src/main/java/io/zipcoder/persistenceapp/controller/HomeController.java
@@ -1,7 +1,7 @@
-package io.zipcoder.persistenceapp.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeController {
-}
+//package io.zipcoder.persistenceapp.controller;
+//
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HomeController {
+//}
--- src/main/java/io/zipcoder/persistenceapp/service/jdbc/HomeService.java
+++ src/main/java/io/zipcoder/persistenceapp/service/jdbc/HomeService.java
@@ -1,8 +1,8 @@
-package io.zipcoder.persistenceapp.service.jdbc;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class HomeService {
-
-}
+//package io.zipcoder.persistenceapp.service.jdbc;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class HomeService {
+//
+//}
