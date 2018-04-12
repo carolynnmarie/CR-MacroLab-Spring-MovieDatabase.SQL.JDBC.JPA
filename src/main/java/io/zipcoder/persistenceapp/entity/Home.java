e45eec4f5cadc8121485826d637b0408f5044db9
Carolynn Vansant <cvansant@MacBook-Pro-7.local>
2018-04-12 16:12:04 -0400

    home changes

--- src/main/java/io/zipcoder/persistenceapp/entity/Home.java
+++ src/main/java/io/zipcoder/persistenceapp/entity/Home.java
@@ -1,31 +1,31 @@
-//package io.zipcoder.persistenceapp.entity;
-//
-//public class Home {
-//    private Integer id;
-//    private String address;
-//    private String home_number;
-//
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
-//
-//    public String getHome_number() {
-//        return home_number;
-//    }
-//
-//    public void setHome_number(String home_number) {
-//        this.home_number = home_number;
-//    }
-//}
+package io.zipcoder.persistenceapp.entity;
+
+public class Home {
+    private Integer id;
+    private String address;
+    private String home_number;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHome_number() {
+        return home_number;
+    }
+
+    public void setHome_number(String home_number) {
+        this.home_number = home_number;
+    }
+}
--- src/main/java/io/zipcoder/persistenceapp/service/jdbc/HomeService.java
+++ src/main/java/io/zipcoder/persistenceapp/service/jdbc/HomeService.java
@@ -1,8 +1,23 @@
-//package io.zipcoder.persistenceapp.service.jdbc;
-//
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class HomeService {
-//
-//}
+package io.zipcoder.persistenceapp.service.jdbc;
+
+import io.zipcoder.persistenceapp.entity.Home;
+import io.zipcoder.persistenceapp.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public int addHome(Home home){
+        return jdbcTemplate.update("INSERT INTO home(address, homenumber) VALUES (?,?)", home.getAddress(), home.getHome_number());
+    }
+
+//    public int addPersonToHome(Person person, Home home){
+//        return jdbcTemplate.update()
+//    }
+
+    public
+}
